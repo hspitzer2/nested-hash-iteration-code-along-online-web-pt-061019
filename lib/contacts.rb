de3bed691abc1dbf3c4a5d2b8f0d76f08911d530
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 21:51:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,8 +13,8 @@ require 'pry'
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-def remove_strawberry
+  
+def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
@@ -23,4 +23,5 @@ def remove_strawberry
         end
       end
     end
+  end
 end
